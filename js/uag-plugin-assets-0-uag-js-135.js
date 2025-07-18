6f1650cfd1583b1f3e03f0b4466b9de92f97ba81
albertogsp <165951837+albertogsp@users.noreply.github.com>
2025-07-18 17:21:59 +0200

    Subida inicial de la web estática

---
+++ js/uag-plugin-assets-0-uag-js-135.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-6f8ae73e .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-6f8ae73e .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-6f8ae73e .swiper-button-next","prevEl":".uagb-block-6f8ae73e .swiper-button-prev"}}	);
+});
+
+ });
